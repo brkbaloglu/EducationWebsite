57f0a4aaa39564f7bea446ee9a69a6a25419c454
Burak Baloglu <burakbaloglu148@gmail.com>
2023-07-26 17:28:30 +0300

    css update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,12 +9,12 @@ function About() {
           <h1 className='text-purple-700 font-bold text-3xl mb-10'>About <span>Us</span></h1>
           <p className='leading-8'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus odit officiis officia, sed eius, fuga iste doloremque harum vel minima tempora atque itaque dolor mollitia asperiores libero tenetur reiciendis, impedit non ipsum corporis maxime doloribus optio? Dolore pariatur vero quasi dolorem facilis neque? Odit voluptatum voluptates modi voluptate et earum?</p>
           <div className='flex items-start mt-6'>
-            <AiFillFacebook size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer'/>
-            <AiOutlineInstagram size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer'/>
-            <AiFillTwitterSquare size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer'/>
-            <AiOutlineWhatsApp size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer'/>
-            <BsPinterest size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer'/>
-            <AiFillLinkedin size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer'/>
+            <AiFillFacebook size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer border-2 border-purple-300'/>
+            <AiOutlineInstagram size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer border-2 border-purple-300'/>
+            <AiFillTwitterSquare size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer border-2 border-purple-300'/>
+            <AiOutlineWhatsApp size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer border-2 border-purple-300'/>
+            <BsPinterest size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer border-2 border-purple-300'/>
+            <AiFillLinkedin size={40} className='mx-1 text-purple-700 hover:bg-purple-700 hover:text-white p-1 rounded-xl cursor-pointer border-2 border-purple-300'/>
           </div>
         </div>
     </div>
